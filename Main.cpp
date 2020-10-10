5fd66ef02aef0f264f70ddd5a4a19aa77c99ff26
Andrey <gusarov2906@gmail.com>
2020-10-10 11:02:01 +0300

    deleted files from main folder(replace to CPP_2019)

--- Main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Menu.h"
-
-int main(int argc, char* argv[])
-{
-	cout << "PURCHASE DEPARTMENT DATABASE by Gusarov A.A." << endl;
-	Database database;
-	Menu menu(&database);
-	menu.run();
-}
